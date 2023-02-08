9b344e98af7732f6a92d5046f2d6f45a8a1a8839
Mario Schirmer <mario.schirmer@ab-sachsen.de>
2023-02-08 15:03:37 +0100

    Initial commit
    Data logger app for USB high temperature sensor

---
+++ USB-data-logger/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Timers;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using USB_data_logger.Models;
+using USB_data_logger.Pheripherals;
+using Timer = System.Timers.Timer;
+
+namespace USB_data_logger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //init UI and controls
+            InitializeComponent();
+
+            //init UI and fill this with data
+            InitApp();
+        }
+        
+        #region Member
+        private DataInformationOs _DataInformationOs;
+            private DataInformationSensor? _DataInformationSensor;
+            private Timer _Timer;
+            private UsbHighTemperatureSensor? _UsbSensor;
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// this method initialize the app and the UI
+        /// </summary>
+        private void InitApp()
+        {
+            //#0 define vars
+            string osInfo = "";
+
+            //#1 read and add App version to UI
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            LblVersion.Content = assembly.GetName().Version;
+            
+            //#3 Create general OS information
+            _DataInformationOs = new DataInformationOs(
+                "Windows " + Environment.OSVersion.VersionString,
+                Environment.UserName,
+                Environment.MachineName,
+                assembly.GetName().Version.ToString());
+
+            //#3.1 display OS information
+            osInfo = "OS: " + _DataInformationOs.OsName + "\n"
+                     + "Username: " + _DataInformationOs.UserName + "\n"
+                     + "Machine name: " + _DataInformationOs.MachineName + "\n"
+                     + "App version: " + _DataInformationOs.AppVersion ;
+            LblOsInformationData.Content = osInfo;
+        }
+
+        /// <summary>
+        /// this method initialize the data acquisition timer
+        /// </summary>
+        private void InitTimer()
+        { 
+            //#0 destroy old timer object
+            if(_Timer != null)
+            {
+                _Timer.Stop();
+                _Timer.Dispose();  
+            }
+                 
+            //#1 config timer 
+            _Timer = new Timer(500);
+            _Timer.Elapsed += TimDataAcq_Tick;
+        }
+
+        /// <summary>
+        /// this method initialize the sensor and the virtual comport
+        /// </summary>
+        private void InitSensor()
+        {
+            //#0 define vars
+            string comportName = "";
+
+            //#1 check selection of combo box
+            if (CboComport.SelectedIndex >= 0)
+            {
+                String errorMessage = "";
+                comportName = CboComport.Text;
+
+                //#2 initialize timer
+                InitTimer();
+
+                //#3 initialise usb device
+                if (_UsbSensor != null) _UsbSensor.Dispose();
+                _UsbSensor = new UsbHighTemperatureSensor();
+                if (_UsbSensor.Init(comportName, ref errorMessage))
+                {
+                    //#3.1 read general information
+                    _UsbSensor.GetSensorInformation(ref _DataInformationSensor);
+
+                    //#3.2 display information
+                    LblSensorInformationData.Content = _DataInformationSensor.RawData;
+                    
+                    //#4 Start data acquisition
+                    _Timer.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Can not initialize application. \n\nError: " + errorMessage, "", MessageBoxButton.OK);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a comport.", "", MessageBoxButton.OK);
+                return;
+            }       
+        }
+        #endregion
+
+        #region Events
+
+            private void BtnClose_Click(object sender, RoutedEventArgs e)
+            {
+                //close app
+                string msg = "Are you sure?";
+                if (MessageBox.Show(msg, "", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    if(_Timer!=null) _Timer.Stop(); 
+                    Close();
+                }
+            }
+
+            private void TimDataAcq_Tick(Object source, ElapsedEventArgs e)
+            {
+                //#0 define vars
+                DataMeasurementSensor? dataMeasurementSensor = null;
+                string outputValue = "";
+                string date = "";
+
+                //#1 read sensor data
+                if (_UsbSensor.GetSensorOutput(ref dataMeasurementSensor))
+                { 
+                    outputValue = dataMeasurementSensor.Output.ToString("F1") + " " + _DataInformationSensor.Unit;
+                    date = dataMeasurementSensor.OutputTimeStamp.ToString("MM/dd/yyyy hh:mm:ss.fff");
+                }
+                else
+                {
+                    date = "Can not read any data.";
+                }
+
+                //#2 display sensor data
+                this.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() => LblSensorOutputCurrentValue.Content = outputValue));
+                this.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() => LblSensorOutputTimeStamp.Content = date));
+            }
+
+            private void BtnInit_Click(object sender, RoutedEventArgs e)
+            {
+                InitSensor();        
+            }
+
+            private void Grid_MouseMove(object sender, MouseEventArgs e)
+            {
+                //move app
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    this.DragMove();
+                }
+            }
+
+            private void CboComport_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+            {
+                //#1 delete old items in combobox
+                CboComport.Items.Clear();
+
+                //#2 add comports to UI
+                foreach (var item in System.IO.Ports.SerialPort.GetPortNames())
+                {
+                    CboComport.Items.Add(item);
+                }
+            }
+            #endregion
+    }
+}
---
+++ USB-data-logger/Pheripherals/UsbHighTemperatureSensor.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Diagnostics;
+using USB_data_logger.Calculations;
+using USB_data_logger.Models;
+
+
+namespace USB_data_logger.Pheripherals
+{
+    public class UsbHighTemperatureSensor
+    {
+
+        /// <summary>
+        /// Com port or serial port for communication between computer and sensor
+        /// </summary>
+        private System.IO.Ports.SerialPort? _SerialPort;
+
+        /// <summary>
+        /// this method initializes the serial port and the temperature sensor
+        /// </summary>
+        /// <param name="portName">name of the serial port, e.g. COM1</param>
+        /// <param name="message">error message in case of initialization issue</param>
+        /// <returns>true -> communication successful established; false -> sometime went wrong, see message for details</returns>
+        public bool Init(string portName, ref string message)
+        {
+            //#00 define vars
+            const int BAUDRATE = 9600; //Baud rate is defined by Sensor firmware
+            DataInformationSensor info = null;
+            var rslt = false;
+
+            //#01 try to open serial port 
+            try
+            {
+                //hint serial port configuration based on sensor manual
+                _SerialPort = new System.IO.Ports.SerialPort();
+                _SerialPort.PortName = portName;
+                _SerialPort.BaudRate = BAUDRATE; //Baud rate define by manual, do not change
+                _SerialPort.DataBits = 8;
+                _SerialPort.StopBits = System.IO.Ports.StopBits.One;
+                _SerialPort.NewLine = "\r"; //\r define by manual
+                _SerialPort.ReadTimeout = 500;
+                _SerialPort.Open();
+
+                //#02 request sensor information to test communication
+                rslt = GetSensorInformation(ref info);
+
+                if (!rslt) message = "No feedback from sensor during initialization process.";
+            }
+            catch (Exception ex)
+            {
+                message = $"Serial port ({portName}) could n´t be initialized. \nError message: {ex.Message}";
+            }
+
+            //#3 if communication test failed, serial port will be closed
+            if (!rslt)
+            {
+                if (_SerialPort != null)
+                {
+                    _SerialPort.Close();
+                    _SerialPort.Dispose();
+                }
+            }
+
+            return rslt;
+        }
+
+        public void Dispose()
+        { 
+            if(_SerialPort!=null) _SerialPort.Dispose();   
+        }
+
+        /// <summary>
+        /// this method requests the temperature of the sensor tip
+        /// </summary>
+        /// <returns>true -> everything is fine; false -> something went wrong</returns>
+        public bool GetSensorOutput(ref DataMeasurementSensor? dataSensor)
+        { 
+            //#0 define vars
+            var rslt = false;
+            var response = "";
+            const string TEMP_REQ = "T?"; //command according product description
+            CalcHelper_UsbSensor calcHelper = new CalcHelper_UsbSensor();
+
+            //#2 request data from sensor
+            if (RequestData(TEMP_REQ, ref response))
+            { 
+               //#3 process and check response
+               rslt = calcHelper.ConvertSensorOutput(response, ref dataSensor);
+            }       
+
+            //#4 return results
+            return rslt;
+        }
+
+        /// <summary>
+        /// this method requests the sensor information
+        /// </summary>
+        /// <param name="dataInformationSensor">class with convert sensor information data</param>
+        /// <returns></returns>
+        public bool GetSensorInformation(ref DataInformationSensor? dataInformationSensor) 
+        {
+            //#0 define vars
+            var rslt = false;
+            var response = "";
+            const string INFO_REQ = "I?"; //command according product description
+            CalcHelper_UsbSensor calcHelper = new CalcHelper_UsbSensor();
+
+            //#2 request data from sensor
+            if (RequestData(INFO_REQ, ref response))
+            {
+                //#3 process and check response
+                rslt = calcHelper.ConvertSensorInformation(response, ref dataInformationSensor);
+            }
+
+            //#4 return results
+            return rslt;
+        }
+
+        /// <summary>
+        /// this method requests data from the sensor 
+        /// </summary>
+        /// <param name="command">command which should by transmitted</param>
+        /// <param name="response">response of the sensor, if response just nothing something went wrong</param>
+        /// <returns>true -> everything was fine, false -> something went wrong</returns>
+        private bool RequestData(string command, ref string response)
+        {
+            //#00 define vars
+            var rslt =false;
+            
+            try
+            {  
+                //#01 send request string
+                _SerialPort.WriteLine(command);  
+
+                //#02 read response
+                response = _SerialPort.ReadLine();
+                rslt = true;
+            }
+            catch (Exception ex)
+            {
+                //error output just in DEBUG mode available
+                #if DEBUG
+                    Debug.WriteLine($"Error occurred during command {command}. Error description: {ex.Message}");
+                #endif
+            }
+
+            //03 return results
+            return rslt;
+        }
+    }
+}
